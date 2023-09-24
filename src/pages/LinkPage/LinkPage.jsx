7f63c28801b652b83268572d2dbc8115e18dc744
James-Lee-01 <freebike01@gmail.com>
2023-09-24 11:23:13 +0800

    feat: add link page

---
+++ src/pages/LinkPage/LinkPage.jsx
@@ -0,0 +1,15 @@
+import styles from './LinkPage.module.scss'
+
+export default function LinkPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.containWrapper}>
+        <p className={styles.title}>Ready for savory memories?</p>
+        <div className={styles.btnWrapper}>
+          <button className={styles.btn +' '+ styles.btnRed}>Browse All Cafes</button>
+          <button className={styles.btn +' '+ styles.btnGreen}>Book Now</button>
+        </div>
+      </div>
+    </div>
+  );
+}
