1d921541d98a16425b85880ee0b45af4e94f97ff
James-Lee-01 <freebike01@gmail.com>
2023-09-24 11:22:38 +0800

    feat: add home page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,18 @@
+import styles from './HomePage.module.scss'
+import Human1 from '../../assets/icons/Humaaans-1.png'
+import Human2 from '../../assets/icons/Humaaans-yellow.png'
+
+export default function HomePage() {
+
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.homeWrapper}>
+        <p className={styles.homeTitle}>RESERVE</p>
+        <div className={styles.iconWrapper}>
+          <img src={Human1} alt='human1' />
+          <img src={Human2} alt='human2' />
+        </div>
+      </div>
+    </div>
+  );
+}
