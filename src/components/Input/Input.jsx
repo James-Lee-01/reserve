b7e3fff29bae4b89d678e24bceaec4b94cf0eac5
James-Lee-01 <freebike01@gmail.com>
2023-09-24 11:24:21 +0800

    feat: add input and button components

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,9 @@
+import styles from './Input.module.scss'
+
+export default function Input({type, placeholder}) {
+  return (
+
+      <input className={styles.input} type={type} placeholder={placeholder} />
+
+  );
+}
