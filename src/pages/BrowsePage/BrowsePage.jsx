bb2d4f0608f70cb19eb56cf90aadbbb831a8f78e
James-Lee-01 <freebike01@gmail.com>
2023-10-24 10:19:41 +0800

    feat: add SearchBar

--- src/pages/BrowsePage/BrowsePage.jsx
+++ src/pages/BrowsePage/BrowsePage.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../components/Navbar/Navbar'
 import Card from '../../components/Card/Card'
 import Pagination from '../../components/Pagination/Pagination'
 import Footer from '../../components/Footer/Footer'
+// import SearchBar from '../../components/SearchBar/SearchBar'
 
 export default function BrowsePage() {
 
@@ -14,11 +15,18 @@ export default function BrowsePage() {
   return (
     <>
       <Navbar />
-      <div className={styles.container}>{cards}</div>
+      <div className={styles.container}>
+        <div className={styles.searchBar}>
+          
+        </div>
+        <div className={styles.cardWrapper}>  
+          {cards}
+        </div>
+      </div>
       <div className={styles.navigator}>
         <Pagination />
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
