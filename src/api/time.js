fff6f73a4f5b13e829a8f1bca292c9237b86e410
James-Lee-01 <freebike01@gmail.com>
2023-10-30 01:52:53 +0800

    feat: add API for cities, time, seats  selection and get all cafes

--- src/api/time.js
+++ src/api/time.js
@@ -4,9 +4,9 @@ const baseUrl = 'https://localhost:3001'
 // const baseUrl = 'https://cafe-reservation-6f0a1b76e65e.herokuapp.com/api'
 
 //time list(user)
-export const getTimes = async () => {
+export const getTimeSlots = async () => {
   try {
-    const { data } = await axios.get(`${baseUrl}/times`)
+    const { data } = await axios.get(`${baseUrl}/times/timeslots`)
     if (data)
     return data
   } catch (error) {
@@ -15,4 +15,14 @@ export const getTimes = async () => {
   }
 }
 
-
+//table list(user)
+export const getSeats = async () => {
+  try {
+    const { data } = await axios.get(`${baseUrl}/tables/seats`)
+    if (data)
+    return data
+  } catch (error) {
+    console.error('[Get Table Failed]', error)
+    return error
+  }
+}
