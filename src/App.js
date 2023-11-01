0fadd078a699d330ce22f8345aaac6eea961ae31
James-Lee-01 <freebike01@gmail.com>
2023-11-01 14:54:27 +0800

    feat: add Page router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import BrowsePage from './pages/BrowsePage/BrowsePage';
 import SingleCafePage from './pages/SingleCafePage/SingleCafePage'
 import LandingPage from './pages/LandingPage/LandingPage';
 import AccountPage from './pages/AccountPage/AccountPage';
+import FavoritePage from './pages/FavoritePage/FavoritePage'
+import StorePage from './pages/StorePage/StorePage'
+import StoreEditPage from './pages/StoreEditPage/StoreEditPage';
+import StoreReservationPage from './pages/StoreReservationPage/StoreReservationPage';
 import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
@@ -24,6 +28,18 @@ function App() {
         }/>
         <Route path='browse/single/cafe/:id' element={
           <SingleCafePage/>
+        }/>
+        <Route path='browse/favorite' element={
+          <FavoritePage/>
+        }/>
+        <Route path='/store' element={
+          <StorePage/>
+        }/>
+        <Route path='/store/edit/:id' element={
+          <StoreEditPage/>
+        }/>
+        <Route path='/store/reservation/:id' element={
+          <StoreReservationPage/>
         }/>
         <Route path='account' element={
           <AccountPage/>
